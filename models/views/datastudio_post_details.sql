85df5d7d6d32249d0cbb6b1c0daa22f00addfd00
lblackley99@gmail.com <lblackley99@gmail.com>
2023-06-28 15:07:40 +0000

    New Datastudio views Added

---
+++ models/views/datastudio_post_details.sql
@@ -0,0 +1,9 @@
+-- dbt model: models/views/datastudio_post_details.sql
+
+-- Create the datastudio_post_details view
+{{ config(materialized='view') }}
+
+SELECT
+id, title, body, creation_date, tag
+  FROM `lydia-sandbox-390714.stackoverflow.posts_questions` ,
+  UNNEST(SPLIT(tags, '|')) AS tag
